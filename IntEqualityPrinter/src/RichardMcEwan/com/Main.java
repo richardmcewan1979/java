5a56a80af9f6e9f6fd8260690a135ba22764015f
richardmcewan1979 <richmcewan@hotmail.com>
2020-10-18 17:35:11 +0100

    Add files via upload

---
+++ IntEqualityPrinter/src/RichardMcEwan/com/Main.java
@@ -0,0 +1,32 @@
+package RichardMcEwan.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printEqual(1,1,1);
+        printEqual(1,1,2);
+        printEqual(-1,-1,-1);
+        printEqual(1,2,3);
+        printEqual(1,2,1);
+    }
+
+    public static void printEqual(int a, int b, int c){
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+        }
+
+        else if ((a == b) && (b == c)){
+            System.out.println("All numbers are equal");
+        }
+
+        else if ((a == b) || (b == c) || (a==c)){
+            System.out.println("Neither all are equal or different");
+        }
+
+        else {
+            System.out.println("All numbers are different");
+        }
+    }
+
+    //end of Main curly
+}
