46d372cdcf1d93193a16d516b7e5fb9e8772c9bf
richardmcewan1979 <richmcewan@hotmail.com>
2020-10-18 18:23:25 +0100

    Add files via upload

---
+++ Switch/src/RichardMcEwan/com/Main.java
@@ -0,0 +1,31 @@
+package RichardMcEwan.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char newChar = 'C';
+
+        switch (newChar){
+            case 'A':
+                System.out.println("Character was A");
+                break;
+            case 'B':
+                System.out.println("Character was B");
+                break;
+            case 'C':
+                System.out.println("Character was C");
+                break;
+            case 'D':
+                System.out.println("Character was D");
+                break;
+            case 'E':
+                System.out.println("Character was E");
+                break;
+            default:
+                System.out.println("Was not A,B,C,D or E");
+        }
+
+
+    }
+}
