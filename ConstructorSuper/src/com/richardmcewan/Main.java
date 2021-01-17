97bf7c48189fcbed59d579b80e040aff380c0cd4
richardmcewan <richmcewan@hotmail.com>
2021-01-17 20:08:10 +0000

    Update Main.java

--- ConstructorSuper/src/com/richardmcewan/Main.java
+++ ConstructorSuper/src/com/richardmcewan/Main.java
@@ -3,6 +3,13 @@ package com.richardmcewan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
+	Shape shape = new Shape(5,7);
+        Rectangle rectangle = new Rectangle(40,70);
+        Rectangle square = new Rectangle(30,30,30,30);
+
+        System.out.println(shape);
+        System.out.println(rectangle);
+        System.out.println(square);
     }
 }
